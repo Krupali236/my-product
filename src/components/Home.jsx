fe30d8055a8d5951a78bbe79bc746f5d21d7588a
krupali <krupali.lalakiya236@gmail.com>
2025-01-24 15:37:47 +0530

    filtered data api completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,7 @@ import {
   Typography,
   CardContent,
   CardMedia,
+  CircularProgress,
   Button,
 } from "@mui/material";
 import { useEffect, useState } from "react";
@@ -15,87 +16,127 @@ import { useEffect, useState } from "react";
 const Home = () => {
   const [category, setCategory] = useState([]);
   const [myData, setMyData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const fetchData = () => {
+    setIsLoading(true);
     fetch("https://fakestoreapi.com/products/categories")
       .then((res) => res.json())
-      .then((json) => setCategory(json));
+      .then((json) => setCategory(json))
+      .catch((error) => {
+        // Handle any errors that occurred during the fetch
+        console.error("Fetch error:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
   useEffect(() => {
     fetchData();
   }, []);
 
   const cardData = () => {
+    setIsLoading(true)
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((json) => setMyData(json));
+      .then((json) => setMyData(json))
+      .catch((error) => {
+        // Handle any errors that occurred during the fetch
+        console.error("Fetch error:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
   console.log(myData, "myData");
   useEffect(() => {
     cardData();
   }, []);
-  const handleChange = (e) => {
-    console.log(e.target.value);
+
+//   const handleChange = (e) => {
+//     const cat = e.target.value;
+//     console.log(cat, "value");
+//   };
+
+  const filteredData = (e) => {
+    setIsLoading(true)
+    const cat = e.target.value;
+    console.log(cat, "value");
+
+    fetch(`https://fakestoreapi.com/products/category/${cat}`)
+      .then((res) => res.json())
+      .then((json) => setMyData(json))
+      .catch((error) => {
+        // Handle any errors that occurred during the fetch
+        console.error("Fetch error:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
-  console.log("category", category);
+  console.log(myData,"Filtered Data");
+
 
   return (
     <>
-      <h1>Home Pcategory</h1>
-      <FormControl fullWidth>
-        <InputLabel
-          id="demo-simple-select-label"
-          style={{ backgroundColor: "white" }}
-        >
-          {" "}
-          products{" "}
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          label="category"
-          onChange={handleChange}
-        >
-          {category.map((v) => {
-            return <MenuItem value={v}>{v}</MenuItem>;
-          })}
-        </Select>
-      </FormControl>
+      {isLoading ? (
+        <div>
+          <CircularProgress />
+        </div>
+      ) : (
+        <div>
+          <FormControl fullWidth>
+            <InputLabel
+              id="demo-simple-select-label"
+              style={{ backgroundColor: "white" }}
+            >
+              {" "}
+              products{" "}
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="category"
+              onChange={filteredData}
+            >
+              {category.map((v) => {
+                return <MenuItem value={v}>{v}</MenuItem>;
+              })}
+            </Select>
+          </FormControl>
 
-      <div className="grid grid-cols-4">
-      {myData.map((ele) => {
-        return (
-            <Card sx={{ minWidth: 275, marginTop: "20px" }}>
-              <CardMedia
-                sx={{ width: "100%" }}
-                component="img"
-                height="400"
-                image={ele.image}
-                alt="Paella dish"
-              />
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  sx={{ color: "text.secondary", fontSize: 14 }}
+          <div className="grid grid-cols-4 gap-4">
+            {myData.map((ele) => {
+              return (
+                <Card
+                  sx={{ minWidth: 275, marginTop: "20px", height: "400px" }}
                 >
-                  {ele.title}
-                </Typography>
-                <Typography variant="h5" component="div"></Typography>
-                <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
-                  {ele.price}
-                </Typography>
-                <Typography variant="body2">
-                  well meaning and kindly.
-                  <br />
-                  {'"a benevolent smile"'}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-        );
-    })}
-    </div>
+                  <CardMedia
+                    sx={{ height: "300px" }}
+                    component="img"
+                    image={ele.image}
+                    alt="Paella dish"
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      sx={{ color: "text.secondary", fontSize: 14 }}
+                    >
+                      {ele.title}
+                    </Typography>
+                    <Typography variant="h5" component="div"></Typography>
+                    <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+                      {ele.price}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </>
   );
 };
